cbcbacf081d18f63c223efc9deb2a9a5dcca29da
John Downey <jtdowney@purdue.edu>
2011-01-11 07:17:28 -0500

    Initial import of iterative robot template

---
+++ src/com/precisionguessworks/frc/Robot2011.java
@@ -0,0 +1,24 @@
+package com.precisionguessworks.frc;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot2011 extends IterativeRobot {
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+    }
+}
