94e6c9e5d6e396f5d34f50faf73c365ac7713631
John Downey <jtdowney@purdue.edu>
2011-01-14 17:56:31 -0500

    Added some basic tank drive functionality

--- src/com/precisionguessworks/frc/Robot2011.java
+++ src/com/precisionguessworks/frc/Robot2011.java
@@ -1,14 +1,24 @@
 package com.precisionguessworks.frc;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.*;
 
 public class Robot2011 extends IterativeRobot {
+    private RobotDrive drive;
+    private Joystick joystick1;
+    private Joystick joystick2;
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-        //Look, a comment!
+        this.joystick1 = new Joystick(1);
+        this.joystick2 = new Joystick(2);
+        this.drive = new RobotDrive(
+                new CANJaguar(2),  // front left
+                new CANJaguar(3),  // rear left
+                new CANJaguar(4),  // front right
+                new CANJaguar(5)); // rear right
     }
 
     /**
@@ -21,5 +31,6 @@ public class Robot2011 extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
+        this.drive.tankDrive(this.joystick1, this.joystick2);
     }
 }
