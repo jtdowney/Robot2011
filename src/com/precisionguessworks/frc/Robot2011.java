205ff309a584a24c3929686a1630f075d1933136
John Downey <jtdowney@purdue.edu>
2011-01-16 18:22:06 -0500

    Renamed joystick fields

--- src/com/precisionguessworks/frc/Robot2011.java
+++ src/com/precisionguessworks/frc/Robot2011.java
@@ -5,16 +5,16 @@ import edu.wpi.first.wpilibj.*;
 public class Robot2011 extends IterativeRobot {
     private DigitalInput input;
     private RobotDrive drive;
-    private Joystick joystick1;
-    private Joystick joystick2;
+    private Joystick leftJoystick;
+    private Joystick rightJoystick;
 
     public void robotInit() {
         System.out.println("Beginning robot initialization");
 
         this.input = new DigitalInput(1);
         this.drive = new RobotDrive(2, 3, 4, 5);
-        this.joystick1 = new Joystick(1);
-        this.joystick2 = new Joystick(2);
+        this.leftJoystick = new Joystick(1);
+        this.rightJoystick = new Joystick(2);
 
         System.out.println("Robot initialized");
     }
@@ -26,6 +26,6 @@ public class Robot2011 extends IterativeRobot {
     }
 
     public void teleopPeriodic() {
-        this.drive.tankDrive(this.joystick1, this.joystick2);
+        this.drive.tankDrive(this.leftJoystick, this.rightJoystick);
     }
 }
