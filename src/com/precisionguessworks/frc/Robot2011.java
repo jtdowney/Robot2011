e2c8c6c842d0fa31e4c0526ce0de5bd771779881
John Downey <jtdowney@purdue.edu>
2011-01-16 16:37:27 -0500

    Updated tank drink to use PWM control for now

--- src/com/precisionguessworks/frc/Robot2011.java
+++ src/com/precisionguessworks/frc/Robot2011.java
@@ -3,33 +3,28 @@ package com.precisionguessworks.frc;
 import edu.wpi.first.wpilibj.*;
 
 public class Robot2011 extends IterativeRobot {
+    private DigitalInput input;
     private RobotDrive drive;
     private Joystick joystick1;
     private Joystick joystick2;
 
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
     public void robotInit() {
+        System.out.println("Beginning robot initialization");
+
+        this.input = new DigitalInput(1);
+        this.drive = new RobotDrive(2, 3, 4, 5);
         this.joystick1 = new Joystick(1);
         this.joystick2 = new Joystick(2);
-        this.drive = new RobotDrive(
-                new CANJaguar(2),  // front left
-                new CANJaguar(3),  // rear left
-                new CANJaguar(4),  // front right
-                new CANJaguar(5)); // rear right
+
+        System.out.println("Robot initialized");
     }
 
-    /**
-     * This function is called periodically during autonomous
-     */
+    public void disabledPeriodic() {
+    }
+
     public void autonomousPeriodic() {
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
         this.drive.tankDrive(this.joystick1, this.joystick2);
     }
