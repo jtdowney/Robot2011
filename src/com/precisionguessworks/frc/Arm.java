6e3d6ab087a0fb95137e39d0f8d6568adbb510e8
John Downey <jtdowney@purdue.edu>
2011-02-12 07:46:41 -0500

    Modified Arm system to explicity use CAN for drive and added a manual drive method

--- src/com/precisionguessworks/frc/Arm.java
+++ src/com/precisionguessworks/frc/Arm.java
@@ -1,20 +1,23 @@
 package com.precisionguessworks.frc;
 
+import edu.wpi.first.wpilibj.CANJaguar;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.PIDSource;
-import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 
 public class Arm {
     private static final double P = 1.0;
     private static final double I = 0.0;
     private static final double D = 0.0;
 
+    private final ArmOutput armOutput;
     private final PIDController controller;
     private final Tower tower;
 
-    public Arm(PIDSource armPotentiometer, SpeedController topMotor, SpeedController bottomMotor, Tower tower) {
-        this.controller = new PIDController(P, I, D, armPotentiometer, new ArmOutput(topMotor, bottomMotor));
+    public Arm(PIDSource armPotentiometer, CANJaguar topMotor, CANJaguar bottomMotor, Tower tower) {
+        this.armOutput = new ArmOutput(topMotor, bottomMotor);
+        this.controller = new PIDController(P, I, D, armPotentiometer, armOutput);
         this.tower = tower;
     }
 
@@ -32,18 +35,47 @@ public class Arm {
         this.setPosition(100);
     }
 
-    public class ArmOutput implements PIDOutput {
-        private SpeedController topMotor;
-        private SpeedController bottomMotor;
+    public void manualDrive(double output) {
+        this.armOutput.drive(output);
+    }
 
-        public ArmOutput(SpeedController topMotor, SpeedController bottomMotor) {
+    protected static double limit(double num) {
+        if (num > 1.0) {
+            return 1.0;
+        }
+        if (num < -1.0) {
+            return -1.0;
+        }
+        
+        return num;
+    }
+
+    public class ArmOutput implements PIDOutput {
+        private CANJaguar topMotor;
+        private CANJaguar bottomMotor;
+
+        public ArmOutput(CANJaguar topMotor, CANJaguar bottomMotor) {
             this.topMotor = topMotor;
             this.bottomMotor = bottomMotor;
         }
 
+        public void drive(double output) {
+            byte syncGroup = (byte) 64;
+
+            double left = Arm.limit(output);
+            double right = -Arm.limit(output);
+
+            try {
+                this.topMotor.setX(left, syncGroup);
+                this.bottomMotor.setX(right, syncGroup);
+
+                CANJaguar.updateSyncGroup(syncGroup);
+            } catch (CANTimeoutException ex) {
+            }
+        }
+
         public void pidWrite(double output) {
-            this.topMotor.set(output);
-            this.bottomMotor.set(output);
+            this.drive(output);
         }
     }
 }
