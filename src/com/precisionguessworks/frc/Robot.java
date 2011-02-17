41619cad8eb9792e35c136ee1be9a00bf8f1dd6f
John Downey <jtdowney@purdue.edu>
2011-02-17 17:33:18 -0500

    Code to output feedback information to CSV file on cRIO

--- src/com/precisionguessworks/frc/Robot.java
+++ src/com/precisionguessworks/frc/Robot.java
@@ -2,17 +2,29 @@ package com.precisionguessworks.frc;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
+import java.io.PrintStream;
+import java.util.Calendar;
+import javax.microedition.io.Connector;
+import com.sun.squawk.microedition.io.FileConnection;
+import com.sun.squawk.util.Arrays;
 
 public class Robot extends IterativeRobot {
+    private AnalogChannel pot;
+    private PrintStream file;
     // inputs
     private LogitechDualActionGamepad leftGamepad;
     private LogitechDualActionGamepad rightGamepad;
 
     // subsystems
     private Claw claw;
+    private CANJaguar topArmMotor;
     private Tower tower;
     private Arm arm;
     private Drive drive;
+    private Compressor compressor;
+
+    private long counter;
+    private int setpoint;
 
     public void robotInit() {
         System.out.println("Initializing robot");
@@ -24,8 +36,8 @@ public class Robot extends IterativeRobot {
         CANJaguar rearLeftMotor;
         CANJaguar frontRightMotor;
         CANJaguar rearRightMotor;
-//        CANJaguar topArmMotor;
-//        CANJaguar bottomArmMotor;
+        //CANJaguar topArmMotor;
+        CANJaguar bottomArmMotor;
 
         System.out.println("Initializing CAN bus");
         while (true) {
@@ -34,8 +46,8 @@ public class Robot extends IterativeRobot {
                 rearLeftMotor = new CANJaguar(2);
                 frontRightMotor = new CANJaguar(3);
                 rearRightMotor = new CANJaguar(4);
-//                topArmMotor = new CANJaguar(6);
-//                bottomArmMotor = new CANJaguar(7);
+                topArmMotor = new CANJaguar(5);
+                bottomArmMotor = new CANJaguar(6);
 
                 break;
             } catch (CANTimeoutException exception) {
@@ -45,9 +57,13 @@ public class Robot extends IterativeRobot {
         System.out.println("CAN bus initialized");
 
         this.claw = new Claw(new DigitalInput(6), new Relay(1), new Relay(2));
-//        this.arm = new Arm(new AnalogChannel(3), topArmMotor, bottomArmMotor, this.tower);
+        pot = new AnalogChannel(3);
+        this.arm = new Arm(pot, topArmMotor, bottomArmMotor, this.tower);
         this.tower = new Tower(new Solenoid(1), new Solenoid(2));
         this.drive = new Drive(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor, new Solenoid(3));
+
+        this.compressor = new Compressor(5, 3);
+        this.compressor.start();
 
         // turn on photoswitches
         new Solenoid(8).set(true);
@@ -67,7 +83,60 @@ public class Robot extends IterativeRobot {
     }
 
     // Autonomous mode
+    public static String paddingString(String s, int n, char c, boolean paddingLeft) {
+        if (s == null) {
+            return s;
+        }
+
+        int add = n - s.length(); // may overflow int size... should not be a problem in real life
+        if(add <= 0) {
+            return s;
+        }
+
+        StringBuffer str = new StringBuffer(s);
+        char[] ch = new char[add];
+        Arrays.fill(ch, c);
+
+        if(paddingLeft){
+            str.insert(0, ch);
+        } else {
+            str.append(ch);
+        }
+        
+        return str.toString();
+    }
+
     public void autonomousInit() {
+        try {
+            Calendar cal = Calendar.getInstance();
+
+            String year = new Integer(cal.get(Calendar.YEAR)).toString();
+            String month = new Integer(cal.get(Calendar.MONTH) + 1).toString();
+            String date = new Integer(cal.get(Calendar.DATE)).toString();
+            String hour = new Integer(cal.get(Calendar.HOUR_OF_DAY)).toString();
+            String minute = new Integer(cal.get(Calendar.MINUTE)).toString();
+            String second = new Integer(cal.get(Calendar.SECOND)).toString();
+            String name =
+                    year +
+                    Robot.paddingString(month, 2, '0', true) +
+                    Robot.paddingString(date, 2, '0', true) + "-" +
+                    Robot.paddingString(hour, 2, '0', true) +
+                    Robot.paddingString(minute, 2, '0', true) +
+                    Robot.paddingString(second, 2, '0', true);
+
+            name += "_" + Arm.kP + "_" + Arm.kI + "_" + Arm.kD + ".csv";
+
+            FileConnection connection = (FileConnection) Connector.open("file:///" + name, Connector.WRITE);
+            connection.create();
+            
+            this.file = new PrintStream(connection.openDataOutputStream());
+        } catch (Exception e) {}
+
+        this.file.println("time,setpoint,value,drive");
+        this.counter = 0;
+        this.setpoint = 300;
+
+        this.arm.setPosition(this.setpoint);
     }
 
     public void autonomousContinuous() {
@@ -75,6 +144,13 @@ public class Robot extends IterativeRobot {
 
     public void autonomousPeriodic() {
         this.updateDashboard();
+
+        try {
+            final String output = (this.counter++) + "," + this.setpoint + "," + this.pot.getValue() + "," + this.topArmMotor.getX();
+
+            this.file.println(output);
+            System.out.println(output);
+        } catch (CANTimeoutException ex) {}
     }
 
     // Teleop mode
@@ -98,7 +174,7 @@ public class Robot extends IterativeRobot {
 
     private void controlArm() {
         if (this.rightGamepad.getNumberedButton(7)) {
-            this.arm.manualDrive(this.rightGamepad.getLeftY());
+            this.arm.manualDrive(this.rightGamepad.getLeftY() * 0.5);
         }
     }
     
