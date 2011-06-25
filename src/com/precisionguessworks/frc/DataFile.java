a1b1507156acc7dcc0b53d2527171b8dcdc53436
John Downey <jtdowney@purdue.edu>
2011-06-25 16:26:16 -0400

    Added constant flag to disable file writing

--- src/com/precisionguessworks/frc/DataFile.java
+++ src/com/precisionguessworks/frc/DataFile.java
@@ -8,37 +8,47 @@ import java.util.Calendar;
 import javax.microedition.io.Connector;
 
 public class DataFile {
+    public final boolean EnableFileWritting = false;
+    
     private final PrintStream file;
 
     public DataFile(String suffix) throws IOException {
-        Calendar cal = Calendar.getInstance();
+        if (EnableFileWritting) {
+            Calendar cal = Calendar.getInstance();
 
-        String year = new Integer(cal.get(Calendar.YEAR)).toString();
-        String month = new Integer(cal.get(Calendar.MONTH) + 1).toString();
-        String date = new Integer(cal.get(Calendar.DATE)).toString();
-        String hour = new Integer(cal.get(Calendar.HOUR_OF_DAY)).toString();
-        String minute = new Integer(cal.get(Calendar.MINUTE)).toString();
-        String second = new Integer(cal.get(Calendar.SECOND)).toString();
-        String name =
-                year +
-                DataFile.paddingString(month, 2, '0', true) +
-                DataFile.paddingString(date, 2, '0', true) + "-" +
-                DataFile.paddingString(hour, 2, '0', true) +
-                DataFile.paddingString(minute, 2, '0', true) +
-                DataFile.paddingString(second, 2, '0', true) + "_" +
-                suffix +
-                ".csv";
+            String year = new Integer(cal.get(Calendar.YEAR)).toString();
+            String month = new Integer(cal.get(Calendar.MONTH) + 1).toString();
+            String date = new Integer(cal.get(Calendar.DATE)).toString();
+            String hour = new Integer(cal.get(Calendar.HOUR_OF_DAY)).toString();
+            String minute = new Integer(cal.get(Calendar.MINUTE)).toString();
+            String second = new Integer(cal.get(Calendar.SECOND)).toString();
+            String name =
+                    year +
+                    DataFile.paddingString(month, 2, '0', true) +
+                    DataFile.paddingString(date, 2, '0', true) + "-" +
+                    DataFile.paddingString(hour, 2, '0', true) +
+                    DataFile.paddingString(minute, 2, '0', true) +
+                    DataFile.paddingString(second, 2, '0', true) + "_" +
+                    suffix +
+                    ".csv";
 
-        FileConnection connection = (FileConnection) Connector.open("file:///" + name, Connector.WRITE);
-        connection.create();
+            FileConnection connection = (FileConnection) Connector.open("file:///" + name, Connector.WRITE);
+            connection.create();
 
-        this.file = new PrintStream(connection.openDataOutputStream());
+            this.file = new PrintStream(connection.openDataOutputStream());
 
-        System.out.println("Writing sensor data to: " + name);
+            System.out.println("Writing sensor data to: " + name);
+        } else {
+            this.file = null;
+            
+            System.out.println("Skipping writting data to file " + suffix + ", renable with DataFile.EnableFileWritting");
+        }
     }
 
     public void writeln(String line) {
-        this.file.println(line);
+        if (EnableFileWritting) {
+            this.file.println(line);
+        }
     }
 
     private static String paddingString(String s, int n, char c, boolean paddingLeft) {
